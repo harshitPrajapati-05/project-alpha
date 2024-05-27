546bc191ed5edf8632aae3eebd0521b458a5f93c
Harshit Prajapati <harshitprajapati5114@gmail.com>
2024-05-27 12:26:50 +0530

    main 1

---
+++ src/app/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+import React, { useEffect, useState } from 'react';
+import { Card } from '@/components/ui/card';
+import { useSession, signIn } from 'next-auth/react';
+import { Session } from 'next-auth';
+import { Button } from '@/components/ui/button';
+
+const Page = () => {
+  const { data: sessionData } = useSession();
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    if (sessionData) {
+      setSession(sessionData as Session);
+    }
+  }, [sessionData]);
+
+  return (
+    <Card className="w-full max-w-md mx-auto space-y-2  text-white/80 font-semibold text-center bg-transparent/80 my-14">
+        <h1> Welcome To Project Alpha </h1>
+        <p> this projects is from Error Guy</p>
+      {session ?
+      <h2>you Started !!!!</h2>
+      :
+      <>
+      <h2> YOU ARE ISN'T SIGNIN</h2>
+      <Button onClick={()=>signIn()}>sign in</Button>
+      </>
+      }
+    </Card>
+  );
+}
+
+export default Page;
