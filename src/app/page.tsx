e3cac085a181556347a6b21509e998f462d950f0
Harshit Prajapati <harshitprajapati5114@gmail.com>
2024-05-27 12:32:00 +0530

    main 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,9 @@ const Page = () => {
         <h1> Welcome To Project Alpha </h1>
         <p> this projects is from Error Guy</p>
       {session ?
+      <>
       <h2>you Started the Journey With Alpha</h2>
+      </>
       :
       <>
       <h2> YOU ARE ISN'T SIGNIN</h2>
