a290c79dcffbb41430887666ad3aa4a75d03a768
Harshit Prajapati <harshitprajapati5114@gmail.com>
2024-06-09 14:39:44 +0530

    changes

--- src/app/api/signout/route.ts
+++ src/app/api/signout/route.ts
@@ -1,5 +1,6 @@
 import { UserModel } from "@/Models/User";
 import { auth } from "@/auth";
+import dbConnect from "@/lib/dbConnect";
 import { User } from "next-auth";
 import { NextResponse } from "next/server";
 
@@ -8,6 +9,7 @@ import { NextResponse } from "next/server";
 
 export const POST = async () => 
     {
+        await    dbConnect();
         const session = await auth();
         const user:User = session?.user;
         const signOuttedUser = await UserModel.findOne({username: user.username});
